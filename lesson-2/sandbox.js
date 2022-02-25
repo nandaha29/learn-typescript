225a46f6eef27636999fb666fc0e6f5aab4d6654
Nanda hafiza <62711982+nandaha29@users.noreply.github.com>
2022-02-25 23:13:37 +0700

    first commit

---
+++ lesson-2/sandbox.js
@@ -0,0 +1,8 @@
+var character = "haie";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+// utk melakukan perulangan dari jumlah element array
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ lesson-2/sandbox.ts
@@ -0,0 +1,10 @@
+const character = "haie";
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+// utk melakukan perulangan dari jumlah element array
+inputs.forEach((input) => {
+  console.log(input);
+});
